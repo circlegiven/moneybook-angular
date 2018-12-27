afefcc49deea146d7dd45ce48c2bb9416a7298e5
circleGiven <circlegiven@gmail.com>
2018-12-27 13:22:35 +0900

    #7 complete test create component
    - use angular/form

--- src/app/test/test-create.component.ts
+++ src/app/test/test-create.component.ts
@@ -1,9 +1,60 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TestService } from '../service/test.service';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'test-create',
   templateUrl: 'test-create.component.html'
 })
-export class TestCreateComponent {
-  
+export class TestCreateComponent implements OnInit {
+  // create input forms
+  testForm: FormGroup;
+
+  constructor(private testService: TestService,
+              private formBuilder: FormBuilder,
+              private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.testForm = this.formBuilder.group({
+      title: ['', [Validators.required, Validators.maxLength(10)]],
+      name: ['', [Validators.required, Validators.maxLength(5)]],
+      description: ['', [Validators.maxLength(100)]],
+    });
+  }
+
+  doneButtonHandler(): void {
+    console.log(this.testForm);
+    console.log(this.testForm.valid);
+    if (this.testForm.valid) {
+      console.log(this.testForm.value.title);
+      console.log(this.testForm.value.name);
+      console.log(this.testForm.value.description);
+    }
+  }
+
+  backButtonHandler(): void {
+    this.router.navigate(['/test']);
+  }
+
+  /**
+   * Set test
+   * @private
+   */
+  private _setTest(): void {
+    this.testService.setTest(this._getTestParams())
+      .subscribe(value => {
+
+      }, error => console.log('error', error));
+  }
+
+  /**
+   * Get test params
+   * @returns {object}
+   * @private
+   */
+  private _getTestParams(): object {
+    return {};
+  }
 }
--- src/app/test/test.module.ts
+++ src/app/test/test.module.ts
@@ -3,9 +3,14 @@ import {TestListComponent} from './test-list.component';
 import {TestCreateComponent} from './test-create.component';
 import {TestDetailComponent} from './test-detail.component';
 import {NgModule} from '@angular/core';
+import { TestService } from '../service/test.service';
+import { CookieService } from 'ngx-cookie-service';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [RouterModule.forChild([
+  imports: [
+    ReactiveFormsModule,
+    RouterModule.forChild([
     {path: '', component: TestListComponent},
     {path: 'create', component: TestCreateComponent},
     {path: ':id', component: TestDetailComponent}
@@ -14,6 +19,7 @@ import {NgModule} from '@angular/core';
     TestListComponent,
     TestCreateComponent,
     TestDetailComponent
-  ]
+  ],
+  providers: [TestService, CookieService]
 })
 export class TestModule { }
